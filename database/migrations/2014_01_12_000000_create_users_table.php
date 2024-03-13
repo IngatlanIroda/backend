369f84a572ca376fc2937f3b6ffd04b3bfd347d9
VassGyongyi <s.v.gyongyi@gmail.com>
2024-03-13 19:20:24 +0100

    szül.idő nullable

--- database/migrations/2014_01_12_000000_create_users_table.php
+++ database/migrations/2014_01_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('name');
-            $table->date('szul_ido')->default(null);
+            $table->date('szul_ido')->nullable();
             $table->foreignId('jogosultsag')
                 ->references('j_id')
                 ->on('jogosultsags'); 
