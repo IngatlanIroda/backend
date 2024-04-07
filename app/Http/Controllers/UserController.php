b93f3343a7f778e34803899f2366e663790d8f02
VassGyongyi <s.v.gyongyi@gmail.com>
2024-04-07 10:05:02 +0200

    userController validáció

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,51 +16,70 @@ class UserController extends Controller
     }
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string',
-            'szul_ido' => 'required|date_format:Y-m-d',
-            'jogosultsag' => 'required|in:user,admin',
-            'aktiv' => 'required|boolean',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
+
         $user = new User();
-        $user->name = $validatedData['name'];
-        $user->szul_ido = $validatedData['szul_ido'];
-        $user->jogosultsag = $validatedData['jogosultsag'];
-        $user->aktiv = $validatedData['aktiv'];
-        $user->email = $validatedData['email'];
-        $user->password = Hash::make($validatedData['password']);
+        $user->name = $request->name;
+        if ($request->szul_ido < now() && $request->szul_ido > '1900-01-01') {
+            $user->szul_ido = $request->szul_ido;
+        } else {
+            return response()->json(['error' => 'Érvénytelen születési idő'], 400);
+        }
+        if ($request->jogosultsag == 'user' || $request->jogosultsag == 'admin') {
+            $user->jogosultsag = $request->jogosultsag;
+        } else {
+            return response()->json(['error' => 'Érvénytelen jogosultság'], 400);
+        }
+
+        if ($request->aktiv == 0 || $request->aktiv == 1) {
+            $user->aktiv = $request->aktiv;
+        } else {
+         
+            return response()->json(['error' => 'Érvénytelen aktiv érték'], 400);
+        }
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
         $user->save();
-        return response()->json(['message' => 'Sikeres regisztráció'], 201);
+        return response()->json(['message' => 'Sikeres művelet'], 201);
     }
 
-    public function show($user_id){
+    public function show($user_id)
+    {
 
         $user = response()->json(User::find($user_id));
         return $user;
     }
     public function destroy($user_id)
     {
-        $user=User::find($user_id);
-        if(!$user->aktiv){
+        $user = User::find($user_id);
+        if (!$user->aktiv) {
             $user->delete();
             return response()->json(['success' => true, 'message' => 'Felhasználó sikeresen törölve!']);
-        }else {
-            return response()->json(['success' => false, 'message' => 'Csak nem aktív felhasználót lehet törölni!']); 
+        } else {
+            return response()->json(['success' => false, 'message' => 'Aktív felhasználót lehet törölni!']);
         }
-       
     }
 
     public function update(Request $request, $user_id)
     {
         $user = User::find($user_id);
-
         $user->user_id = $request->user_id;
         $user->name = $request->name;
+        if ($request->szul_ido < now() && $request->szul_ido > '1900-01-01') {
+            $user->szul_ido = $request->szul_ido;
+        } else {
+            return response()->json(['error' => 'Érvénytelen születési idő'], 400);
+        }
         $user->szul_ido = $request->szul_ido;
-        $user->jogosultsag = $request->jogosultsag;
-        $user->aktiv = $request->aktiv;
+        if ($request->jogosultsag == 'user' || $request->jogosultsag == 'admin') {
+            $user->jogosultsag = $request->jogosultsag;
+        } else {
+            return response()->json(['error' => 'Érvénytelen jogosultság'], 400);
+        }
+        if ($request->aktiv == 0 || $request->aktiv == 1) {
+            $user->aktiv = $request->aktiv;
+        } else {
+            return response()->json(['error' => 'Érvénytelen aktiv érték'], 400);
+        }
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
         $user->save();
@@ -68,7 +87,7 @@ class UserController extends Controller
     public function userTable()
     {
         $userList = DB::table('users')
-            ->select('user_id', 'name', 'szul_ido', 'jogosultsag', 'aktiv', 'email','password')
+            ->select('user_id', 'name', 'szul_ido', 'jogosultsag', 'aktiv', 'email', 'password')
 
             ->get();
         return $userList;
